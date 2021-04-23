61940d203ef7ba26c5a8c53c8f8144618b642415
muhammedaydogan <muhammed.aydogan@aol.com>
2021-04-23 18:36:54 +0300

    image links refreshed

--- src/services/restaurants/mock/index.js
+++ src/services/restaurants/mock/index.js
@@ -11,11 +11,17 @@ export const mocks = {
 };
 
 export const mockImages = [
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-baking-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table-600x400.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg",
+  "https://i0.hippopx.com/photos/799/284/990/breakfast-dinner-egg-food-preview.jpg",
+  "https://i0.hippopx.com/photos/779/699/611/tomato-salad-food-by-the-sea-food-with-a-view-outdoor-food-preview.jpg",
+  "https://cdn.pixabay.com/photo/2020/05/11/15/06/food-5158707_960_720.jpg",
+  "https://c.pxhere.com/photos/8c/ab/beef_bread_bun_burger_calories_cheese_cheeseburger_close_up-1546505.jpg!d",
+  "https://i0.hippopx.com/photos/287/127/671/food-sushi-restaurant-japanese-food-preview.jpg",
+  "https://i0.hippopx.com/photos/353/29/249/flan-sweet-delicious-dessert-preview.jpg",
+  "https://i0.hippopx.com/photos/797/1015/608/mexican-food-burrito-mexican-food-preview.jpg",
+  "https://i0.hippopx.com/photos/471/708/981/food-sushi-restaurant-japanese-food-preview.jpg",
+  "https://i0.hippopx.com/photos/869/797/216/macaroon-personalise-pastry-food-and-drink-preview.jpg",
+  "https://i0.hippopx.com/photos/470/159/792/meat-mutton-roll-food-chinese-food-preview.jpg",
+  "https://i0.hippopx.com/photos/336/84/994/strawberry-milk-strawberry-milk-green-preview.jpg",
+  "https://i0.hippopx.com/photos/875/321/300/raspberry-berry-ripe-berries-of-a-raspberry-preview.jpg",
+  "https://i0.hippopx.com/photos/474/233/233/blueberries-dessert-daisy-fruit-preview.jpg",
 ];
